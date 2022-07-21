1f30c35363faad85911f27ed6f7f6823ee621693
Satishpokala124 <satishpokala124@gmail.com>
2022-07-21 22:34:15 +0530

    Added custom_id_generator() to MyUserManager class.

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,22 +1,30 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.forms import EmailField
+from django.db.models import Max
 from django.utils.translation import gettext_lazy as _
 
 
 class MyUserManager(UserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         if not email or not len(email.strip()):
-            raise TypeError(f"'email' can't be None")
+            raise TypeError("email can't be None or empty")
         if not password or not len(password.strip()):
-            raise TypeError(f"'password' can't be None")
+            raise TypeError("password can't be None or empty")
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("id", self.custom_id_generator())
         return self._create_user(username, email, password, **extra_fields)
+
+    def custom_id_generator(self):
+        prefix = f'{self.model._meta.app_label[0]}{self.model._meta.object_name[0]}'
+        id_max = int(self.aggregate(id_max=Max('id'))['id_max'].strip(prefix))
+        id_max = str(id_max+1).rjust(15, '0')
+        new_id = f'{prefix}{id_max}'
+        return new_id
 
 
 class User(AbstractUser):
-    id = models.CharField(primary_key=True, max_length=20)
+    id = models.CharField(primary_key=True, max_length=20, null=False, blank=False, editable=False)
     first_name = models.CharField(_("first name"), max_length=150, blank=False)
     last_name = models.CharField(_("last name"), max_length=150, blank=False)
     email = models.EmailField(_("email address"), blank=False, unique=True)
