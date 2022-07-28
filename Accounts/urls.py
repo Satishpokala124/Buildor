cec7c332f49941e95422493eb57feac7fde942fd
Satishpokala124 <satishpokala124@gmail.com>
2022-07-28 20:43:02 +0530

    Added names to url paths.

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Accounts import views
 
 urlpatterns = [
-    path('login/', views.login,),
-    path('register/', views.register),
-    path('logout/', views.logout)
+    path('login/', views.login, name='user_login'),
+    path('register/', views.register, name='user_register'),
+    path('logout/', views.logout, name='user_logout')
 ]
